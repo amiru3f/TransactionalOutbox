5555d55978915dc48b7dee5b7ccebd121b9a1a55
amirsolhi <a.solhi@alibaba.ir>
2023-02-10 19:15:06 +0330

    cleanup

--- TransactionalOutbox/Program.cs
+++ TransactionalOutbox/Program.cs
@@ -6,7 +6,7 @@ using TransactionalOutbox.Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<NotifDbContext>(options =>
 {
-    options.UseSqlServer("server=localhost;database=test;User Id=sa;Password=123456aA;trust server certificate=true");
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NotificationDb"));
 });
 
 if (!builder.Environment.IsEnvironment("Test"))
@@ -20,7 +20,7 @@ builder.Services.AddSingleton<IBus, MockBus>();
 var app = builder.Build();
 
 
-app.MapGet("/", (NotifDbContext context) => context.Notifications.FirstOrDefault().CreatedAt);
+app.MapGet("/", (NotifDbContext context) => context.Notifications.FirstOrDefault()?.CreatedAt);
 
 app.Run();
 
